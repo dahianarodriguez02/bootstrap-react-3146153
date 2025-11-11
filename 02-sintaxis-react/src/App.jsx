d42c1bea392e03b0e137843b521cd904f972f0f2
dahianarodriguez02 <murillolesly02@gmail.com>
2025-11-11 13:17:38 -0500

    seccion card terminada

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -61,7 +61,24 @@ export const App = () => {
     <h1>React + Boostrap</h1>
     <LoginForm/>
 
-    <ProductCard/>
+     <div className="container">
+      <div className="row">
+
+        <div className=" col-12 col-md-6 col-lg-4">
+          <ProductCard/>
+        
+        </div>
+        <div className="col-12 col-md-6 col-lg-4 mt-4 mt-md-0">
+          <ProductCard/>
+        
+        </div>
+        <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+          <ProductCard/>
+        
+        </div>
+      </div>
+     </div>
+    
     </>
   )
 }
