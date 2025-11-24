c4f0795b1b91a5ad621e4ebe60531cce712839ca
dahianarodriguez02 <murillolesly02@gmail.com>
2025-11-24 13:23:00 -0500

    creacion Glitchverse

---
+++ 04-Glitch-Verse/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavBar } from './components/NavBar'
+import { ComicCard } from './components/ComicCard'
+
+
+export const App = () => {
+  return (
+    <>
+        <NavBar/>
+        <ComicCard/>
+    </>
+  )
+}
---
+++ 04-Glitch-Verse/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Styles/NavBar.css"
+
+export const NavBar = () => {
+  return (
+    <div>
+        <img src="." alt="" />
+    </div>
+  )
+}
